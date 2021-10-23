084f61312f1836c2564a4e6da7a842caf52ffa07
elijahharry <elijah@outbounded.com>
2021-10-23 05:14:24 -0500

    Fixing logo

--- src/components/Titles/Titles.js
+++ src/components/Titles/Titles.js
@@ -4,7 +4,7 @@ import styles from "../../../styles/Titles.module.css";
 const Titles = ({ title, subtitle }) => {
   return (
     <>
-      <img src="/img/eh.png" className={styles.logo} />
+      <img src="/img/eh.jpg" className={styles.logo} />
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.description}>{subtitle}</p>
     </>
