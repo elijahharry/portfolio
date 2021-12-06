2ebd1bc48263e8ed77f8c7069a3cc26bd1b36a56
Elijah Harry <elijahharry@Elijahs-MacBook-Pro.local>
2021-12-06 04:57:47 -0600

    Adding flash effect to gradient

--- src/components/Portfolio/Gradient/Gradient.js
+++ src/components/Portfolio/Gradient/Gradient.js
@@ -3,22 +3,37 @@ import classes from "./Gradient.module.css";
 
 const Gradient = ({ primary, secondary }) => {
   const [switched, setSwitched] = useState(false);
+  const [count, setCount] = useState(0);
   const [current, setCurrent] = useState({
+    primary: primary,
+    secondary: secondary,
+  });
+  const [previous, setPrevious] = useState({
     primary: primary,
     secondary: secondary,
   });
 
   useEffect(() => {
     setSwitched(true);
+    setPrevious({
+      primary: current.primary,
+      secondary: current.secondary,
+    });
     const timer = setTimeout(() => {
       setCurrent({
         primary: primary,
         secondary: secondary,
       });
       setSwitched(false);
-    }, 650);
+    }, 500);
     return () => clearTimeout(timer);
   }, [primary, secondary]);
+
+  useEffect(() => {
+    if (switched) {
+      setCount(count + 1);
+    }
+  }, [switched]);
 
   return (
     <div className={classes.container}>
@@ -35,6 +50,15 @@ const Gradient = ({ primary, secondary }) => {
         className={classes.overlay}
         style={{
           opacity: switched ? 1 : 0,
+          background: `linear-gradient(217deg, ${previous.primary}, ${previous.secondary} 30.71%, ${previous.primary} 70%),
+          linear-gradient(127deg, ${previous.secondary}, ${previous.primary} 70.71%),
+          linear-gradient(336deg, ${previous.primary}, ${previous.secondary} 70.71%)`,
+        }}
+      />
+      <span
+        className={classes.flash}
+        style={{
+          transform: `translateX(${count % 2 === 0 ? "-" : ""}100%)`,
         }}
       />
     </div>
