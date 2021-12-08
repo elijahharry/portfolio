d41cf1866fb97c48adba642fccb41e6a5121d390
Elijah Harry <elijah@elijahharry.com>
2021-12-08 01:40:54 -0600

    Favicon

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -15,6 +15,29 @@ const Header = () => {
         href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
       />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/fav/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/fav/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/fav/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/fav/site.webmanifest" />
+      <link rel="mask-icon" href="/fav/safari-pinned-tab.svg" color="#222222" />
+      <link rel="shortcut icon" href="/fav/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#2a2a2a" />
+      <meta name="msapplication-config" content="/fav/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 };
