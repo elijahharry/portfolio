bb7a84ee359cb0b77881e61e12054ad92580e085
Elijah Harry <elijah@elijahharry.com>
2022-02-26 23:04:59 -0600

    Attempting to fix loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { useTheme } from "@context/Theme";
 
 import classes from "./Loader.module.css";
 
 const Loader = () => {
   const [loaded, setLoaded] = useState(false);
+  const theme = useTheme();
 
   useEffect(() => {
     const loader = setTimeout(() => setLoaded(true), 2000);
@@ -14,7 +16,11 @@ const Loader = () => {
     return <></>;
   } else {
     return (
-      <section className={classes.loader}>
+      <section
+        className={`${classes.loader} ${
+          theme?.dark ? classes.loader_dark : classes.loader_light
+        }`}
+      >
         {/* <img src="/img/eh.svg" /> */}
       </section>
     );
